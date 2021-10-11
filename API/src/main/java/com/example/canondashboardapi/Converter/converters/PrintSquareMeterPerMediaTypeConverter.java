531022fd1e3276edb314e043823263dad9aab2ce
Kevin <i439586@fhict.nl>
2021-10-11 13:53:04 +0200

    Made folder for the converters

---
+++ API/src/main/java/com/example/canondashboardapi/Converter/converters/PrintSquareMeterPerMediaTypeConverter.java
@@ -0,0 +1,4 @@
+package com.example.canondashboardapi.Converter.converters;
+
+public class PrintSquareMeterPerMediaTypeConverter {
+}
---
+++ API/src/main/java/com/example/canondashboardapi/Converter/interfaces/IGraphConverter.java
@@ -0,0 +1,4 @@
+package com.example.canondashboardapi.Converter.interfaces;
+
+public interface IGraphConverter <TModel, TDto>{
+}
