a853056e8db29779b88d8950ff05c16890143dba
Kevin <i439586@fhict.nl>
2021-10-11 12:36:48 +0200

    Initialized Project CanonDashboardAPI

---
+++ API/src/main/java/com/example/canondashboardapi/CanonDashboardApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.canondashboardapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CanonDashboardApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CanonDashboardApiApplication.class, args);
+    }
+
+}
