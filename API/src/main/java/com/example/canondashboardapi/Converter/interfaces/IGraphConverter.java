83f55e9fdedd963ca765c898cb5539c59d5174be
Eli Brouwers <eli.brouwers@student.fontys.nl>
2021-10-12 13:43:17 +0200

    Add abstract methods to IGraphConverter

--- API/src/main/java/com/example/canondashboardapi/Converter/interfaces/IGraphConverter.java
+++ API/src/main/java/com/example/canondashboardapi/Converter/interfaces/IGraphConverter.java
@@ -1,4 +1,10 @@
 package com.example.canondashboardapi.Converter.interfaces;
 
+import java.util.List;
+
 public interface IGraphConverter <TModel, TDto>{
+
+    TDto modelToDTO(TModel model);
+
+    List<TDto> modelToDTO(List<TModel> models);
 }
