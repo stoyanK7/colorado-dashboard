6df81908b3e5a304602ba9d51771307dd04f0c48
Kevin <i439586@fhict.nl>
2021-10-13 16:12:44 +0200

    Create some persistent test data - for 2 days for every category - Local Repository

--- API/src/main/java/com/example/canondashboardapi/repository/repositories/PrintSquareMeterPerMediaCategoryRepository.java
+++ API/src/main/java/com/example/canondashboardapi/repository/repositories/PrintSquareMeterPerMediaCategoryRepository.java
@@ -21,10 +21,32 @@ public class PrintSquareMeterPerMediaCategoryRepository implements IGenericRepos
         this.totalPrintSquareMeterPerMediaCategoryPerDays = new ArrayList<>();
         String sDate1="31/12/1998";
         Date date1=new SimpleDateFormat("dd/MM/yyyy").parse(sDate1);
-        TotalPrintSquareMeterPerMediaCategoryPerDay totalPrintSquareMeterPerMediaCategoryPerDay1 = new TotalPrintSquareMeterPerMediaCategoryPerDay(1, date1, 1, MediaCategory.Canvas);
-        TotalPrintSquareMeterPerMediaCategoryPerDay totalPrintSquareMeterPerMediaCategoryPerDay2 = new TotalPrintSquareMeterPerMediaCategoryPerDay(2, date1, 1, MediaCategory.Thick_film);
-        this.totalPrintSquareMeterPerMediaCategoryPerDays.add(totalPrintSquareMeterPerMediaCategoryPerDay1);
-        this.totalPrintSquareMeterPerMediaCategoryPerDays.add(totalPrintSquareMeterPerMediaCategoryPerDay2);
+        String sDate2="31/12/1999";
+        Date date2=new SimpleDateFormat("dd/MM/yyyy").parse(sDate2);
+
+        this.totalPrintSquareMeterPerMediaCategoryPerDays.add(new TotalPrintSquareMeterPerMediaCategoryPerDay(1, date1, 1, MediaCategory.Film));
+        this.totalPrintSquareMeterPerMediaCategoryPerDays.add(new TotalPrintSquareMeterPerMediaCategoryPerDay(2, date1, 2, MediaCategory.Light_paper));
+        this.totalPrintSquareMeterPerMediaCategoryPerDays.add(new TotalPrintSquareMeterPerMediaCategoryPerDay(3, date1, 3, MediaCategory.Heavy_paper));
+        this.totalPrintSquareMeterPerMediaCategoryPerDays.add(new TotalPrintSquareMeterPerMediaCategoryPerDay(4, date1, 4, MediaCategory.Light_Banner));
+        this.totalPrintSquareMeterPerMediaCategoryPerDays.add(new TotalPrintSquareMeterPerMediaCategoryPerDay(5, date1, 5, MediaCategory.Textile));
+        this.totalPrintSquareMeterPerMediaCategoryPerDays.add(new TotalPrintSquareMeterPerMediaCategoryPerDay(6, date1, 6, MediaCategory.Monomeric_vinyl));
+        this.totalPrintSquareMeterPerMediaCategoryPerDays.add(new TotalPrintSquareMeterPerMediaCategoryPerDay(7, date1, 7, MediaCategory.Canvas));
+        this.totalPrintSquareMeterPerMediaCategoryPerDays.add(new TotalPrintSquareMeterPerMediaCategoryPerDay(8, date1, 8, MediaCategory.Polymeric_and_cast_vinyl));
+        this.totalPrintSquareMeterPerMediaCategoryPerDays.add(new TotalPrintSquareMeterPerMediaCategoryPerDay(9, date1, 9, MediaCategory.Heavy_Banner));
+        this.totalPrintSquareMeterPerMediaCategoryPerDays.add(new TotalPrintSquareMeterPerMediaCategoryPerDay(10, date1, 10, MediaCategory.Paper));
+        this.totalPrintSquareMeterPerMediaCategoryPerDays.add(new TotalPrintSquareMeterPerMediaCategoryPerDay(11, date1, 11, MediaCategory.Thick_film));
+
+        this.totalPrintSquareMeterPerMediaCategoryPerDays.add(new TotalPrintSquareMeterPerMediaCategoryPerDay(12, date2, 1, MediaCategory.Film));
+        this.totalPrintSquareMeterPerMediaCategoryPerDays.add(new TotalPrintSquareMeterPerMediaCategoryPerDay(13, date2, 2, MediaCategory.Light_paper));
+        this.totalPrintSquareMeterPerMediaCategoryPerDays.add(new TotalPrintSquareMeterPerMediaCategoryPerDay(14, date2, 3, MediaCategory.Heavy_paper));
+        this.totalPrintSquareMeterPerMediaCategoryPerDays.add(new TotalPrintSquareMeterPerMediaCategoryPerDay(15, date2, 4, MediaCategory.Light_Banner));
+        this.totalPrintSquareMeterPerMediaCategoryPerDays.add(new TotalPrintSquareMeterPerMediaCategoryPerDay(16, date2, 5, MediaCategory.Textile));
+        this.totalPrintSquareMeterPerMediaCategoryPerDays.add(new TotalPrintSquareMeterPerMediaCategoryPerDay(17, date2, 6, MediaCategory.Monomeric_vinyl));
+        this.totalPrintSquareMeterPerMediaCategoryPerDays.add(new TotalPrintSquareMeterPerMediaCategoryPerDay(18, date2, 7, MediaCategory.Canvas));
+        this.totalPrintSquareMeterPerMediaCategoryPerDays.add(new TotalPrintSquareMeterPerMediaCategoryPerDay(19, date2, 8, MediaCategory.Polymeric_and_cast_vinyl));
+        this.totalPrintSquareMeterPerMediaCategoryPerDays.add(new TotalPrintSquareMeterPerMediaCategoryPerDay(20, date2, 9, MediaCategory.Heavy_Banner));
+        this.totalPrintSquareMeterPerMediaCategoryPerDays.add(new TotalPrintSquareMeterPerMediaCategoryPerDay(21, date2, 10, MediaCategory.Paper));
+        this.totalPrintSquareMeterPerMediaCategoryPerDays.add(new TotalPrintSquareMeterPerMediaCategoryPerDay(22, date2, 11, MediaCategory.Thick_film));
     }
 
     public List<TotalPrintSquareMeterPerMediaCategoryPerDay> getAll() {
