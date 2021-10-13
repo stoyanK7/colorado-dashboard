8eefe4b9a4d417cf1431f17ac6ae1ff4a5531585
Kevin <i439586@fhict.nl>
2021-10-13 15:44:14 +0200

    Controller works with new converter

--- API/src/main/java/com/example/canondashboardapi/controller/PrintSquareMeterPerMediaCategoryController.java
+++ API/src/main/java/com/example/canondashboardapi/controller/PrintSquareMeterPerMediaCategoryController.java
@@ -1,12 +1,16 @@
 package com.example.canondashboardapi.controller;
 
-import com.example.canondashboardapi.model.models.PrintSquareMeterPerMediaCategory;
-import com.example.canondashboardapi.service.interfaces.IPrintSquareMeterPerMediaCategoryService;
+
+import com.example.canondashboardapi.converter.interfaces.IGraphConverter;
+import com.example.canondashboardapi.model.models.TotalPrintSquareMeterPerMediaCategoryPerDay;
+import com.example.canondashboardapi.service.interfaces.ITotalPrintSquareMeterPerMediaCategoryPerDayService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 // TODO: Need to change later
@@ -15,33 +19,32 @@ import java.util.List;
 @CrossOrigin("http://localhost:4000")
 public class PrintSquareMeterPerMediaCategoryController {
 
-//    @Autowired
-//    IGraphConverter graphConverter;
+    @Autowired
+    IGraphConverter<List<TotalPrintSquareMeterPerMediaCategoryPerDay>,List<Map<String, String>>> graphConverter;
 
     @Autowired
-    IPrintSquareMeterPerMediaCategoryService printSquareMeterPerMediaService;
+    ITotalPrintSquareMeterPerMediaCategoryPerDayService printSquareMeterPerMediaCategoryPerDayService;
 
 
     @GetMapping()
     // change to TotalPrintSquareMeterPerMediaCategoryPerDay
-    public ResponseEntity<List<PrintSquareMeterPerMediaCategory>> getAll(){
-//        List<PrintSquareMeterPerMediaTypeDTO> printSquareMeterPerMediaTypeDTOList = graphConverter.entityToDto(printSquareMeterPerMediaService.getAll());
-//        if (printSquareMeterPerMediaTypeDTOList != null){
-//            return ResponseEntity.ok().body(printSquareMeterPerMediaTypeDTOList);
-//        }else{
-//            return new ResponseEntity("No data found.", HttpStatus.NOT_FOUND);
-//        }
-        return ResponseEntity.ok().body(printSquareMeterPerMediaService.getAll());
+    public ResponseEntity<List<Map<String, String>>> getAll(){
+        List<Map<String, String>> graphDayBars = graphConverter.modelToDTO(printSquareMeterPerMediaCategoryPerDayService.getAll());
+        if (graphDayBars != null){
+            return ResponseEntity.ok().body(graphDayBars);
+        }else{
+            return new ResponseEntity("No data found.", HttpStatus.NOT_FOUND);
+        }
     }
 
     @PostMapping("/saveTest")
-    public void saveTest(@RequestBody PrintSquareMeterPerMediaCategory testType){
+    public void saveTest(@RequestBody TotalPrintSquareMeterPerMediaCategoryPerDay testType){
 //        List<PrintSquareMeterPerMediaTypeDTO> printSquareMeterPerMediaTypeDTOList = graphConverter.entityToDto(printSquareMeterPerMediaService.getAll());
 //        if (printSquareMeterPerMediaTypeDTOList != null){
 //            return ResponseEntity.ok().body(printSquareMeterPerMediaTypeDTOList);
 //        }else{
 //            return new ResponseEntity("No data found.", HttpStatus.NOT_FOUND);
 //        }
-        printSquareMeterPerMediaService.saveTest(testType);
+        printSquareMeterPerMediaCategoryPerDayService.saveTest(testType);
     }
 }
