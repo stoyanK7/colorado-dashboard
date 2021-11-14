121f416433ecd97794ff7e7722794eea41e1a575
stoyan <s.kostadinov@student.fontys.nl>
2021-11-14 22:08:06 +0100

    Added filters

--- Frontend/project-canon/src/components/site/View.jsx
+++ Frontend/project-canon/src/components/site/View.jsx
@@ -1,14 +1,40 @@
 import '../../css/View.css';
 
+import { faCalendarAlt, faExpand } from '@fortawesome/free-solid-svg-icons'
+
 import Chart from '../shared/Chart';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Header from '../static/Header';
 
 const View = () => {
   return (
-    <div className="view">
+    <div className='view'>
       <Header />
       <main>
-        1D, 1W , some inputs, full screen button, whatever
+        <div className='filters'>
+          <div className='bins'>
+            <span className='one-day'>1D</span>
+            <span className='one-week'>1W</span>
+          </div>
+          <div className='timespan'>
+            <div className='from'>
+              <input type='date' />
+              <FontAwesomeIcon icon={faCalendarAlt} className='fa-calendar-alt' />
+            </div>
+            <span>to</span>
+            <div className='to'>
+             <input type='date' />
+              <FontAwesomeIcon icon={faCalendarAlt} className='fa-calendar-alt' />
+            </div>
+          </div>
+          <div className='specific-printers'>
+            <input type='text' placeholder='Specific printers..' />
+          </div>
+          <div className='full-screen'>
+            <FontAwesomeIcon icon={faExpand} className='fa-expand' />
+            <span>Full screen</span>
+          </div>
+        </div>
         <Chart />
       </main>
     </div>
