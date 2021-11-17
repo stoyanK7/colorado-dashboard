6b4ceac76592b465bc6f0bba168082667dd81d55
Valeri Videv <valeri.videv@abv.bg>
2021-11-17 20:11:26 +0100

    finished home page

--- Frontend/project-canon/src/components/shared/Chart.jsx
+++ Frontend/project-canon/src/components/shared/Chart.jsx
@@ -2,21 +2,27 @@ import Loading from '../static/Loading';
 import MediaCategoryBarChart from '../charts/MediaCategoryBarChart';
 import useFetch from '../../hooks/useFetch';
 import { useParams } from 'react-router-dom';
-
-const Chart = () => {
-  const { chart } = useParams();
-
-  const { data, isPending, error } = useFetch(`/${chart}`);
+//sasdasdasasdasdasdasdfsgrwe
+const Chart = (props) => {
+  var { chart } = useParams();
+  var chartToUse;
+  if(props.manualChart != undefined){
+    chartToUse = props.manualChart;
+  }
+  else{
+    chartToUse = chart;
+  }
+  const { data, isPending, error } = useFetch(`/${chartToUse}`);
 
   let component;
-  switch (chart) {
+  switch (chartToUse) {
     case 'PrintSquareMeterPerMediaType': component = <MediaCategoryBarChart data={data} index='date' />; break;
     // TODO: add the rest of the paths when the API has them
     default: break;
   }
 
   return (
-    <div className='chart' style={{ height: '100vh' }}>
+    <div className='chart' style={{ height: 'auto'}}>
       {isPending && <Loading />}
       {error && <h1>An error occured: {error}</h1>}
       {data && component}
--- Frontend/project-canon/src/components/site/Home.jsx
+++ Frontend/project-canon/src/components/site/Home.jsx
@@ -1,11 +1,35 @@
 import { Link } from "react-router-dom";
+import classes from "./../../css/home_page.module.css";
+import Chart from "../shared/Chart";
 
 const Home = () => {
   return (
-    <div className='home'>
-      <Link to='/PrintSquareMeterPerMediaType'>Print square meter per media type</Link>
+    <div className={classes.home}>
+      <div className={classes.container}>
+        <div className={classes.headerHome}>Colorado</div>
+        <div className={`${classes.mainGraph} ${classes.graph}`}>
+        <div className={classes.headerGraph}>Printed square metres per media type</div>
+          <div className={classes.image}></div>
+        </div>
+        <div className={`${classes.sideGraphtop} ${classes.graph}`}>
+        <div className={classes.headerGraph}>Amount of printed square metres per print mode</div>
+          <div className={classes.image}></div>
+        </div>
+        <div className={`${classes.sideGrapthMiddle} ${classes.graph}`}>
+        <div className={classes.headerGraph}>Ink usage</div>
+          <div className={classes.image}></div>
+        </div>
+        <div className={`${classes.secondLineLeftGraph} ${classes.graph}`}>
+        <div className={classes.headerGraph}> Best printing machines on the market</div>
+          <div className={classes.image}></div>
+        </div>
+        <div className={`${classes.secondLineRightGraph} ${classes.graph}`}>
+        <div className={classes.headerGraph}>All used media types for a selected machine</div>
+          <div className={classes.image}></div>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default Home;
