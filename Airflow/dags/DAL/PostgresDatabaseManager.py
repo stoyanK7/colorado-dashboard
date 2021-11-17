48ccc71f35c7490451e55c2d22778879d3a98373
elibr <i477782@fhict.nl>
2021-11-17 16:14:31 +0100

    Add postgresDatabaseManager object

---
+++ Airflow/dags/DAL/PostgresDatabaseManager.py
@@ -0,0 +1,7 @@
+from airflow.providers.postgres.operators.postgres import PostgresHook
+import pandas as pd
+
+
+class PostgresDatabaseManager:
+    def __init__(self):
+        self.hook = PostgresHook(postgres_conn_id='postgres_default')
