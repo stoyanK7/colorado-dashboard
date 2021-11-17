9d07b2b771f330234f31c6ea2ec5a21c2f5716a7
elibr <i477782@fhict.nl>
2021-11-17 20:46:05 +0100

    Add create table function

--- Airflow/dags/DAL/PostgresDatabaseManager.py
+++ Airflow/dags/DAL/PostgresDatabaseManager.py
@@ -5,3 +5,6 @@ import pandas as pd
 class PostgresDatabaseManager:
     def __init__(self):
         self.hook = PostgresHook(postgres_conn_id='postgres_default')
+
+    def createTable(self, dataFrame: pd.DataFrame, tableName: str):
+        dataFrame.to_sql(tableName, con=self.hook.get_sqlalchemy_engine(), if_exists="replace")
