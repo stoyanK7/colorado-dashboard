634b2cd7dc501fd7b00924e04527f661e87ba7bb
elibr <i477782@fhict.nl>
2021-11-18 14:57:22 +0100

    Fix default none parameter for create table

--- Airflow/dags/DAL/PostgresDatabaseManager.py
+++ Airflow/dags/DAL/PostgresDatabaseManager.py
@@ -1,3 +1,4 @@
+import sqlalchemy
 from airflow.providers.postgres.operators.postgres import PostgresHook
 import pandas as pd
 from sqlalchemy import text
@@ -7,7 +8,7 @@ class PostgresDatabaseManager:
     def __init__(self):
         self.hook = PostgresHook(postgres_conn_id='postgres_default')
 
-    def createTable(self, dataFrame: pd.DataFrame, tableName: str, conn: sqlalchemy.engine.Engine):
+    def createTable(self, dataFrame: pd.DataFrame, tableName: str, conn: sqlalchemy.engine.Engine = None):
         if conn == None:
             conn = self.hook.get_sqlalchemy_engine()
         dataFrame.to_sql(tableName, con=conn, if_exists="replace")
