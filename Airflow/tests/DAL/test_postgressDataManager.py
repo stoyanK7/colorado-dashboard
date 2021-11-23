e3998e4201fb5490fcbcb3294820870956ea8daf
elibr <i477782@fhict.nl>
2021-11-23 17:15:28 +0100

    Update insertIntoTable command

--- Airflow/tests/DAL/test_postgressDataManager.py
+++ Airflow/tests/DAL/test_postgressDataManager.py
@@ -18,7 +18,7 @@ class PostgresDatabaseManagerTests(unittest.TestCase):
         dbm = PostgresDatabaseManager()
         engine = create_engine("sqlite:////repo/unittests.db")
 
-        dbm.createTable(tableName=name, dataFrame=dataFrame, conn=engine)
+        dbm.insertIntoTable(tableName=name, dataFrame=dataFrame, conn=engine)
 
         with engine.connect() as connection:
             result = connection.execute(text("select * from testdataframe;"))
