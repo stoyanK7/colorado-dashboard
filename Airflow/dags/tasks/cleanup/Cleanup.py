7cc13f2e7dd6a51ae422eb4457a9f6485a24f24e
I467491 <i467491@fhict.nl>
2021-11-24 14:26:28 +0100

    pipeline-cleanup task

---
+++ Airflow/dags/tasks/cleanup/Cleanup.py
@@ -0,0 +1,54 @@
+import logging
+
+import pandas as pd
+
+from config import AggregateTableNameConfig, \
+    CleanTableNameConfig, \
+    LastSeenTableConfig, \
+    ReadTableNameConfig, \
+    LastSeenColumnNameConfig
+
+from DAL import PostgresDatabaseManager
+
+
+class CleanupTasks:
+
+    @staticmethod
+    def cleanup(ti):
+        # Clean the data from the databases
+        CleanupTasks._cleanup_tables(AggregateTableNameConfig)
+        CleanupTasks._cleanup_tables(CleanTableNameConfig)
+        CleanupTasks._cleanup_tables(LastSeenTableConfig)
+        CleanupTasks._cleanup_tables(ReadTableNameConfig)
+
+        # save from X com into the database
+        CleanupTasks._xcom_to_db(ti,
+                                 LastSeenColumnNameConfig.LAST_SEEN_IMAGE_FILE_PATH,
+                                 LastSeenColumnNameConfig.LAST_SEEN_IMAGE_ROW_ID,
+                                 LastSeenTableConfig.LAST_SEEN_IMAGE_TABLE)
+
+    @staticmethod
+    def _cleanup_tables(table_name_config):
+        pdm = PostgresDatabaseManager()
+
+        # take all table_names from module table_name_config and clean them up
+        for name in table_name_config.__dict__:
+            if not name.startswith('_'):
+                # print(getattr(table_name_config, name)) for testing
+                table_name = getattr(table_name_config, name)
+
+                logging.info(f"Cleaning the table {table_name}.")
+
+                pdm.deleteTable(table_name)
+
+    @staticmethod
+    def _xcom_to_db(ti, c1_name, c2_name, table_name):
+        logging.info(f"Saving last seen file and last seen row in table {table_name}.")
+
+        last_seen_file = ti.xcom_pull(task_ids='readImage', key='lastSeenFile')  # to change task_id
+        last_seen_row = ti.xcom_pull(task_ids='readImage', key='lastSeenRow')  # to change task_id
+
+        pdm = PostgresDatabaseManager()
+
+        df = pd.DataFrame({c1_name: [last_seen_file], c2_name: [last_seen_row]})
+        pdm.insertIntoTable(df, table_name)
