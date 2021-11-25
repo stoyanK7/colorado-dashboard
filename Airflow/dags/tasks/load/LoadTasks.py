ecf99aa43caf52c4903e38b8ecb45b16cae120a7
Kevin <i439586@fhict.nl>
2021-11-25 13:19:28 +0100

    airflow-pipeline load task

--- Airflow/dags/tasks/load/LoadTasks.py
+++ Airflow/dags/tasks/load/LoadTasks.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 from config import AggregateTableNameConfig, AggregateColumnNameConfig
 from DAL.PostgresDatabaseManager import PostgresDatabaseManager
-
+from tabulate import tabulate
 
 class LoadTasks:
 
@@ -13,7 +13,10 @@ class LoadTasks:
     @staticmethod
     def LoadMediaCategoryUsage():
         df = LoadTasks.__read_from_db_postgresql(AggregateTableNameConfig.AGGREGATEIMAGE)
-        LoadTasks.__add_data_to_api(df)
+        if df.empty:
+            logging.info("No new data was found, skipping step.")
+            return
+
 
         first_date_df = LoadTasks.__get_first_value_from_df(df, AggregateColumnNameConfig.DATE)
         first_area_df = LoadTasks.__get_first_value_from_df(df, AggregateColumnNameConfig.IMAGEAREA)
@@ -23,13 +26,21 @@ class LoadTasks:
 
 
 
-        b_check_adding_area = LoadTasks.__check_adding_area(last_date_api, first_date_df)
-        new_area = 0
-        if (b_check_adding_area):
-            new_area = LoadTasks.__adding_area(last_area_api, first_area_df)
+
+        if last_area_api != "" and last_date_api != "":
+            if LoadTasks.__check_adding_area(last_date_api, first_date_df):
+                new_area = LoadTasks.__adding_area(last_area_api, first_area_df)
+                LoadTasks.__update_area('media_category_usage', new_area, last_date_api)
+                df = df[df[AggregateColumnNameConfig.DATE] != pd.to_datetime(last_date_api.strftime('%Y-%m-%d'), format='%Y-%m-%d')]
+                logging.info(tabulate(df, headers='keys', tablefmt='psql'))
+                logging.info(new_area)
+        logging.info(df)
+        LoadTasks.__add_data_to_api(df)
+        logging.info(last_date_api)
+        logging.info(first_date_df)
         logging.info(last_area_api)
         logging.info(first_area_df)
-        logging.info(new_area)
+
 
 
 
@@ -56,7 +67,10 @@ class LoadTasks:
             return cursor.fetchone()[0]
         except pymysql.Error as error:
             logging.info(error)
-            return
+            return ""
+        except TypeError as error:
+            logging.info(error)
+            return ""
         finally:
             logging.info("close the database connection using close() method.")
             connection.close()
@@ -72,10 +86,6 @@ class LoadTasks:
         last_date_api = last_date_api.strftime('%Y-%m-%d')
         logging.info(last_date_api)
         logging.info(last_date_df)
-        logging.info("Checking is null")
-        if last_date_api == "":
-            return False
-
         logging.info("Checking is equal")
         if last_date_api == last_date_df:
             return True
@@ -91,28 +101,26 @@ class LoadTasks:
 
 
     @staticmethod
-    def __update_area(table_name, new_area):
+    def __update_area(table_name, new_area, date):
         try:
             logging.info("Making connection with MySql database")
             logging.info("Adding data to the  MySql database")
+            logging.info(date.strftime('%Y-%m-%d'))
+            date = date.strftime('%Y-%m-%d')
             connection = pymysql.connect(host='host.docker.internal', user='canon', password='canon', db='canon')
             cursor = connection.cursor()
 
-            sql_read_query = """WITH imagetable AS
-                                ( SELECT TOP 1 * FROM {table} BY date DESC)
-                                UPDATE {table} SET printed_square_meters={area};"""\
-                                .format(table=table_name, area=new_area)
-            cursor = connection.cursor(prepared=True)
+            sql_read_query = """UPDATE {table} SET printed_square_meters={area} WHERE date ='{date}';"""\
+                                .format(table=table_name, area=new_area,date=date)
+            logging.info(sql_read_query)
             cursor.execute(sql_read_query)
-            return cursor.fetchall()
+            connection.commit()
         except pymysql.Error as e:
             logging.info(e)
             return
         finally:
-            if connection.is_connected():
-                cursor.close()
-                connection.close()
-                logging.info("MySql connection is closed")
+            logging.info("close the database connection using close() method.")
+            connection.close()
 
 
 
@@ -127,12 +135,12 @@ class LoadTasks:
             cols = "`,`".join([str(i) for i in df.columns.tolist()])
 
             for i, row in df.iterrows():
-                sql = "INSERT INTO `media_category_usage` " \
-                      "(machine_id, `" +cols + "`)" \
-                      " VALUES (0, " + "%s," * (len(row) - 1) + "%s)"
                 # sql = "INSERT INTO `media_category_usage` " \
-                #         "(machine_id, date, media_category, printed_square_meters)" \
-                #         " VALUES (0," + "%s,"*(len(row)-1) + "%s)"
+                #       "(machine_id, `" +cols + "`)" \
+                #       " VALUES (0, " + "%s," * (len(row) - 1) + "%s)"
+                sql = "INSERT INTO `media_category_usage` " \
+                        "(machine_id, date, media_category, printed_square_meters)" \
+                        " VALUES (0," + "%s,"*(len(row)-1) + "%s)"
                 cursor.execute(sql, tuple(row))
                 connection.commit()
         except pymysql.Error as e:
