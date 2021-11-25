b2192f3fd8e4f03b6eb533900d2a9a97860f6964
stoyan <s.kostadinov@student.fontys.nl>
2021-11-25 09:55:59 +0100

    Return InkUsage ordered by date.

--- API/src/main/java/api/coloradodashboard/inkusage/JpaInkUsageRepository.java
+++ API/src/main/java/api/coloradodashboard/inkusage/JpaInkUsageRepository.java
@@ -2,5 +2,8 @@ package api.coloradodashboard.inkusage;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface JpaInkUsageRepository extends JpaRepository<InkUsage, Long> {
+    List<InkUsage> getAllByOrderByDateAsc();
 }
