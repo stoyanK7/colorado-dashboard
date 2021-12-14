25b587527b801b45095674fb96f108859c1e2e5e
elibr <i477782@fhict.nl>
2021-12-14 22:06:55 +0100

    Fix clean and aggregation syntax

--- Airflow/dags/config/clean_table_name_config.py
+++ Airflow/dags/config/clean_table_name_config.py
@@ -1,3 +1,3 @@
-READ_IMAGE = "cleanimage"
-READ_MEDIA_PREPARE = "cleanimage"
-READ_PRINT_CYCLE = "cleanimage"
+READ_IMAGE = "clean_image"
+READ_MEDIA_PREPARE = "clean_media_prepare"
+READ_PRINT_CYCLE = "clean_print_cycle"
--- Airflow/dags/config/read_table_name_config.py
+++ Airflow/dags/config/read_table_name_config.py
@@ -1,3 +1,3 @@
-READ_IMAGE = "readimage"
+READ_IMAGE = "read_image"
 READ_MEDIA_PREPARE = "read_media_prepare"
 READ_PRINT_CYCLE = "read_print_cycle"
