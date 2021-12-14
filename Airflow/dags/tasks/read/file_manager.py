73a6c6585b7175b18536bb24d7702c918181da3e
elibr <i477782@fhict.nl>
2021-12-14 19:50:35 +0100

    Fix missing slash

--- Airflow/dags/tasks/read/file_manager.py
+++ Airflow/dags/tasks/read/file_manager.py
@@ -6,10 +6,5 @@ import shutil
 class FileManager:
 
     def copy_files_to_dir(self, source_dir, target_dir, extension_to_folder):
-        shutil.copytree(source_dir + extension_to_folder, target_dir + extension_to_folder)
-
-if __name__ == '__main__':
-    file_manager = FileManager()
-    # file_manager.copy_files_to_dir("C:/Users/elibr/Documents/Fontys/GPS/Data",
-    #                                "C:/Users/elibr/Documents/Fontys/GPS/DataCopy")
-    print(file_manager.get_different_files("C:/Users/elibr/Documents/Fontys/GPS/DataCopy", "C:/Users/elibr/Documents/Fontys/GPS/Data", "/engineControl/accounting/image"))
+        for machine_id in os.listdir(source_dir):
+            shutil.copytree(source_dir + "/" + machine_id + extension_to_folder, target_dir + "/" + machine_id + extension_to_folder)
