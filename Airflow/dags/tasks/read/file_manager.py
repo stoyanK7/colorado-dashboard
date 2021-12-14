b34202ad9ef5bc422775dcaa0ea43d72e3f75a01
elibr <i477782@fhict.nl>
2021-12-14 10:55:07 +0100

    Move getting different files to fileReader

--- Airflow/dags/tasks/read/file_manager.py
+++ Airflow/dags/tasks/read/file_manager.py
@@ -5,27 +5,11 @@ import shutil
 
 class FileManager:
 
-    def copy_files_to_dir(self, source_dir, target_dir):
-        shutil.copytree(source_dir, target_dir)
+    def copy_files_to_dir(self, source_dir, target_dir, extension_to_folder):
+        shutil.copytree(source_dir + extension_to_folder, target_dir + extension_to_folder)
 
-    def get_different_files(self, new_dir, old_dir, path_to_files):
-        new_files = {}
-        changed_files = {}
-        # for every machine id
-        for machine_id in os.listdir(new_dir):
-            # make empty lists
-            new_files[machine_id] = []
-            changed_files[machine_id] = []
-            for root, dirs, files in os.walk(new_dir + "/" + machine_id + path_to_files):
-                # for every file in the new files directory
-                for file in files:
-                    # if the file doesn't exist in the old directory, add it to the new_files list
-                    if not os.path.isfile(old_dir + "/" + machine_id + path_to_files + '/' + file):
-                        new_files[machine_id].append(new_dir + "/" + machine_id + path_to_files + '/' + file)
-
-                    # else if it does exist, check if it is different. If so, add it to the changed_files list
-                    elif not filecmp.cmp(new_dir + "/" + machine_id + path_to_files + '/' + file, old_dir + "/" + machine_id + path_to_files + '/' + file):
-                        changed_files[machine_id].append({"new": new_dir + "/" + machine_id + path_to_files + '/' + file,
-                                                         "old":old_dir + "/" + machine_id + path_to_files + '/' + file})
-
-        return new_files, changed_files
+if __name__ == '__main__':
+    file_manager = FileManager()
+    # file_manager.copy_files_to_dir("C:/Users/elibr/Documents/Fontys/GPS/Data",
+    #                                "C:/Users/elibr/Documents/Fontys/GPS/DataCopy")
+    print(file_manager.get_different_files("C:/Users/elibr/Documents/Fontys/GPS/DataCopy", "C:/Users/elibr/Documents/Fontys/GPS/Data", "/engineControl/accounting/image"))
--- Airflow/dags/tasks/read/file_reader.py
+++ Airflow/dags/tasks/read/file_reader.py
@@ -1,3 +1,4 @@
+import filecmp
 import logging
 import os
 import pandas as pd
@@ -20,3 +21,25 @@ class FileReader():
 
     def read_pandas_csv_file(self, path, delimiter):
         return pd.read_csv(path, delimiter=delimiter)
+
+    def get_different_files(self, new_dir, old_dir, path_to_files):
+        new_files = {}
+        changed_files = {}
+        # for every machine id
+        for machine_id in os.listdir(new_dir):
+            # make empty lists
+            new_files[machine_id] = []
+            changed_files[machine_id] = []
+            for root, dirs, files in os.walk(new_dir + "/" + machine_id + path_to_files):
+                # for every file in the new files directory
+                for file in files:
+                    # if the file doesn't exist in the old directory, add it to the new_files list
+                    if not os.path.isfile(old_dir + "/" + machine_id + path_to_files + '/' + file):
+                        new_files[machine_id].append(new_dir + "/" + machine_id + path_to_files + '/' + file)
+
+                    # else if it does exist, check if it is different. If so, add it to the changed_files list
+                    elif not filecmp.cmp(new_dir + "/" + machine_id + path_to_files + '/' + file, old_dir + "/" + machine_id + path_to_files + '/' + file):
+                        changed_files[machine_id].append({"new": new_dir + "/" + machine_id + path_to_files + '/' + file,
+                                                         "old":old_dir + "/" + machine_id + path_to_files + '/' + file})
+
+        return new_files, changed_files
