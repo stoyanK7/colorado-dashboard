9bec6562bb6b4e410a44dd79e10756096516d7bf
stoyan <s.kostadinov@student.fontys.nl>
2021-12-24 18:44:06 +0200

    Top machines with most print volume chart ready✅

--- Frontend/src/components/charts/TopMachinesWithMostPrintVolumeBarChart.jsx
+++ Frontend/src/components/charts/TopMachinesWithMostPrintVolumeBarChart.jsx
@@ -1,7 +1,40 @@
+import {
+  Bar,
+  BarChart,
+  CartesianGrid,
+  Legend,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis
+} from 'recharts';
+
 import React from 'react';
 
-const TopMachinesWithMostPrintVolumeBarChart = ({ data, index }) => {
-  return (<div></div>);
-}
+const TopMachinesWithMostPrintVolumeBarChart = ({ data }) => {
+  return (
+    <ResponsiveContainer width='100%' height='100%'>
+      <BarChart
+        data={data}
+        margin={{
+          top: 15,
+          right: 50,
+          left: 30,
+          bottom: 50
+        }}
+      >
+        <CartesianGrid strokeDasharray='3 3' />
+        <XAxis dataKey='Printer id' textAnchor='start' angle={40} />
+        <YAxis unit='SqM' type='number'
+        // TODO: set this with maxvalue to fix overflowing labels
+        // domain={[0, maxValue]} 
+        />
+        <Tooltip />
+        <Legend verticalAlign='top' iconType='circle' />
+        <Bar dataKey='Printed square meters' stackId='a' fill='#333333' />
+      </BarChart>
+    </ResponsiveContainer>
+  );
+};
 
 export default TopMachinesWithMostPrintVolumeBarChart;
