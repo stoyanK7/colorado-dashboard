5733b3a8db08de7bdd8093d6e12a028b060a4006
stoyan <s.kostadinov@student.fontys.nl>
2021-12-24 21:19:16 +0200

    Media types per machine chart ready✅

--- Frontend/src/components/charts/MediaTypesPerMachineBarChart.jsx
+++ Frontend/src/components/charts/MediaTypesPerMachineBarChart.jsx
@@ -1,7 +1,42 @@
+import {
+  Bar,
+  BarChart,
+  CartesianGrid,
+  Legend,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis
+} from 'recharts';
+
 import React from 'react';
 
-const MediaTypesMerMachineBarChart = ({ data, index }) => {
-    return (<div></div>);
+const MediaTypesMerMachineBarChart = ({ data }) => {
+  return (
+    <ResponsiveContainer width='100%' height='100%'>
+      <BarChart
+        width={700}
+        height={300}
+        data={data}
+        margin={{
+          top: 20,
+          right: 50,
+          left: 40,
+          bottom: 50
+        }}
+      >
+        <CartesianGrid strokeDasharray='3 3' />
+        <XAxis dataKey='Media type' textAnchor='start' angle={40} />
+        <YAxis unit='SqM' type='number'
+        // TODO: set this with maxvalue to fix overflowing labels
+        // domain={[0, maxValue]} 
+        />
+        <Tooltip />
+        <Legend verticalAlign='top' iconType='circle' />
+        <Bar dataKey='Printed square meters' stackId='a' fill='#69283A' />
+      </BarChart>
+    </ResponsiveContainer>
+  );
 };
 
 export default MediaTypesMerMachineBarChart;
