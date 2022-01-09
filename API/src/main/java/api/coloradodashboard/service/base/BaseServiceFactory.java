4a15430f75f5a9f3c2500b72aa13dff02f7d1583
Kostadinov,Stoyan S.L <s.kostadinov@student.fontys.nl>
2022-01-09 13:19:46 +0000

    Add sonarqube✅

--- API/src/main/java/api/coloradodashboard/service/base/BaseServiceFactory.java
+++ API/src/main/java/api/coloradodashboard/service/base/BaseServiceFactory.java
@@ -8,6 +8,6 @@ import org.springframework.stereotype.Service;
 @Service
 public class BaseServiceFactory<T> {
     public BaseService<T> getBaseService(BaseRepository<T> repository) {
-        return new BaseService<T>(repository);
+        return new BaseService<>(repository);
     }
 }
