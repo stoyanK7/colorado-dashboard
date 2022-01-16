7c5366468b03ff3aa0eac1c2f0ba0e355dc0315d
elibr <i477782@fhict.nl>
2022-01-16 23:29:43 +0100

    Add skip to preprocess steps when no new data is available

--- Airflow/dags/tasks/preprocess/preprocess_tasks.py
+++ Airflow/dags/tasks/preprocess/preprocess_tasks.py
@@ -43,6 +43,11 @@ class PreprocessTasks():
         # Take the dataframe from the previous step
         df = PreprocessTasks._read_from_db(clean_table_name_config.READ_IMAGE)
 
+        # Skip if no new data
+        if df.empty:
+            logging.info("No new data was found, skipping step.")
+            return
+
         # Columns to be removed from the table
         columns_to_drop = [clean_image_col_name_constants.IMAGE_LENGTH, clean_image_col_name_constants.IMAGE_WIDTH,
                            clean_image_col_name_constants.MEDIA_TYPE, clean_image_col_name_constants.LOCAL_TIME]
@@ -67,6 +72,11 @@ class PreprocessTasks():
         # Take the dataframes from mediaPrepare and PrintCycle table
         df1 = PreprocessTasks._read_from_db(clean_table_name_config.READ_MEDIA_PREPARE)
         df2 = PreprocessTasks._read_from_db(clean_table_name_config.READ_PRINT_CYCLE)
+
+        # Skip if no new data
+        if df1.empty and df2.empty:
+            logging.info("No new data was found, skipping step.")
+            return
 
         # Merge the dataframes
         df = PreprocessTasks._merge_two_dataframes(df1, df2, clean_print_cycle_col_name_constants.ENGINE_CYCLE_ID)
@@ -115,6 +125,11 @@ class PreprocessTasks():
         logging.info("Start preprocess for media category usage.")
         # Take the dataframe from the previous step
         df = PreprocessTasks._read_from_db(clean_table_name_config.READ_PRINT_CYCLE)
+
+        # Skip if no new data
+        if df.empty:
+            logging.info("No new data was found, skipping step.")
+            return
 
         # Columns to be removed from the table
         columns_to_drop = [clean_print_cycle_col_name_constants.LOCAL_TIME, clean_print_cycle_col_name_constants.ENGINE_CYCLE_ID,
