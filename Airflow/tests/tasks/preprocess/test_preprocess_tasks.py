0b0ab1020559e44a97354319aa1ed1ec8c1dba22
Valeri Videv <valeri.videv@abv.bg>
2022-01-17 23:33:58 +0100

    added read_step new functionality unit testing

--- Airflow/tests/tasks/preprocess/test_preprocess_tasks.py
+++ Airflow/tests/tasks/preprocess/test_preprocess_tasks.py
@@ -12,7 +12,45 @@ from config import clean_table_name_config, clean_image_col_name_constants, aggr
     aggregate_table_name_config, clean_print_cycle_col_name_constants, preprocess_col_name_constants
 
 
-class TestAggregateTasks(unittest.TestCase):
+class TestPreprocessTasks(unittest.TestCase):
+
+    def test_converting_units_to_default_values(self):
+        df = pd.DataFrame(data={"ullid": [0, 1, 2, 3],
+                                "AccountedInkBlack": [3, 3, 3, 3],
+                                "AccountedInkBlack|unit|": ["cl", "cl", "cl", "cl"]})
+
+        expected = pd.DataFrame(data={"ullid": [0, 1, 2, 3],
+                                "AccountedInkBlack": [0.03, 0.03, 0.03, 0.03]})
+
+        actual = PreprocessTasks._converting_units_to_default_values(df, [])
+
+        pd.testing.assert_frame_equal(actual, expected)
+
+    def test_removing_unnecessary_columns(self):
+        df = pd.DataFrame(data={"ullid": [0, 1, 2, 3],
+                                "AccountedInkBlack": [3, 3, 3, 3],
+                                "AccountedInkBlack|unit|": ["cl", "cl", "cl", "cl"],
+                                "SmtTest": ["value", "value", "value", "value"]})
+
+        expected = pd.DataFrame(data={"ullid": [0, 1, 2, 3],
+                                "AccountedInkBlack": [0.03, 0.03, 0.03, 0.03]})
+
+        columns_to_remove = ["SmtTest"]
+        actual = PreprocessTasks._converting_units_to_default_values(df, columns_to_remove)
+
+        pd.testing.assert_frame_equal(actual, expected)
+
+    def test_convert_date_to_utc(self):
+        df = pd.DataFrame(data={"ullid": [0],
+                                "date": ["2022-01-17"]})
+
+        expected = pd.DataFrame(data={"ullid": [0],
+                                "date": ["2022-01-17"]})
+
+        actual = PreprocessTasks._convert_date_to_utc(df)
+
+        pd.testing.assert_frame_equal(actual, expected)
+
 
     def test_divide_column_by(self):
         data = pd.DataFrame(data={clean_print_cycle_col_name_constants.SQUARE_DECIMETER: ["3", "2", "1"]})
